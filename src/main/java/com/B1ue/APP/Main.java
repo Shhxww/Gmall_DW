595e7fdd68187643c84895d75da875c5d55f1743
Shhxww <912631419@qq.com>
2025-04-08 10:29:46 +0800

    init project

---
+++ src/main/java/com/B1ue/APP/Main.java
@@ -0,0 +1,10 @@
+package com.B1ue.APP;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/main/java/com/B1ue/Main.java
@@ -0,0 +1,10 @@
+package com.B1ue;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
